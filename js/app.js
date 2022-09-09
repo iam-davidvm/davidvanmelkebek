4f2f27ca2c6cde0753d62bdb8cb2e6967452b679
iam-davidvm <vanmelkebekedavid@gmail.com>
2022-09-10 00:38:16 +0200

    creating some kind of pagination part 1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 let selectedCategory = 'all';
 let searchTerm = '';
+const paginationNumbers = document.getElementById("pagination-numbers"); // NAV
+const nextButton = document.getElementById("next-button"); // NAV
+const prevButton = document.getElementById("prev-button"); // NAV
+const paginationLimit = 6; // NAV
+let currentPage; // NAV
+let pageNum = 1;
 
 /* get json file */
 
@@ -20,47 +26,108 @@ getPosts();
 
 /* end of get json file */
 
+/* Pagination */
+const appendPageNumber = (index) => {
+    const pageNumber = document.createElement("button");
+    // pageNumber.className = "pagination";
+    pageNumber.classList.add("pagination","pagination-number");
+    pageNumber.innerHTML = index;
+    pageNumber.setAttribute("page-index", index);
+    pageNumber.setAttribute("aria-label", "Page " + index);
+   
+    paginationNumbers.appendChild(pageNumber);
+  };
+ /* end of Pagination */
+
 /* render page */
 
 function renderPage(posts) {
     
+    /* change category */
     if (selectedCategory !== 'all') {
         posts = posts.filter(item => {
             return item.category === selectedCategory;
         })
     }
 
+    /* end of change category */
+
+    /* search title */
     if (searchTerm !== '') {
         posts = posts.filter(item => {
             return item.title.toLowerCase().includes(searchTerm.toLowerCase());
         })
     }
+    /* end search title */
+    
 
+
+    /* TODO */
+    // REFACTOR PAGINATION CODE
+    // SEARCH DOESN'T WORK ANYMORE
+    // CATEGORY FILTER DOESN'T SHOW UNIQUE VALUES
+
+
+
+
+
+    /* Pagination */
+    const pageCount = Math.ceil(posts.length / paginationLimit); // NAV
+
+    paginationNumbers.innerHTML = '';
+
+    for (let i = 1; i <= pageCount; i++) {
+        appendPageNumber(i);
+    }
+
+    
+    currentPage = pageNum;
+    
+    const prevRange = (pageNum - 1) * paginationLimit;
+    const currRange = pageNum * paginationLimit;
+
+    document.querySelectorAll(".pagination-number").forEach((button) => {
+        console.log(button)
+        button.classList.remove("active");
+        const pageIndex = Number(button.getAttribute("page-index"));
+        if (pageIndex === currentPage) {
+          button.classList.add("active");
+          button.disabled = true;
+        }
+      });
+
+
+    posts = posts.slice(prevRange, currRange);
+    console.log(posts);
+
+    /* End of Pagination */
+    
     let html = '';
     posts.forEach(post => {
         const postArticle = `<article class="post">
         <figure>
-            <a href="/pages/index.html?art=${post.random}" class="overlay">
-                <div class="center">
-                                    <p>Read More</p>
-                                    <i class="fa-solid fa-eye fa-xl"></i>
-                </div>
-            </a>
-            <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80" alt="${post.title}">
+        <a href="/pages/index.html?art=${post.random}" class="overlay">
+        <div class="center">
+        <p>Read More</p>
+        <i class="fa-solid fa-eye fa-xl"></i>
+        </div>
+        </a>
+        <img src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80" alt="${post.title}">
         </figure>
         <p><span class="category-pill">${post.category}</span></p>
         <h1>${post.title}</h1>
         <p>${post.teaser}</p>
-    </article>`;
-
+        </article>`;
+        
         html += postArticle;
     });
-
+    
     const htmlPosts = document.querySelector('#posts');
     htmlPosts.innerHTML = html;
     if (posts.length === 0) {
         htmlPosts.innerText = 'No posts available';
     }
+
 }
 
 /* end of render page */
@@ -111,3 +178,14 @@ searchTitles.addEventListener('input', () => {
 })
 
 /* end search for title */
+
+/* change page */
+
+document.querySelector('#pagination-numbers').addEventListener('click', (e) => {
+    if(e.target.getAttribute('page-index')) {
+        pageNum = Number(e.target.getAttribute('page-index'));
+        getPosts();
+    };
+});
+
+/* end of change page */
