84aeb758db418178f860cb7ea2de69c452b12439
iam-davidvm <vanmelkebekedavid@gmail.com>
2022-09-13 00:53:15 +0200

    trying to solve multiple line issues, found possible solution

--- pagemaker/app.js
+++ pagemaker/app.js
@@ -1,7 +1,7 @@
 const addButton = document.getElementById('add-button');
 const editButton = document.getElementById('edit-button');
+const submitButton = document.getElementById('publish');
 const addSection = document.getElementById('add');
-
 
 addButton.addEventListener('click', () => {
     const buttons = document.getElementById('buttons');
@@ -30,25 +30,43 @@ addTitle.addEventListener('input', () => {
 });
 
 
-const addMarkUp = (element) => {
-    const sel = document.getSelection();
+const addMarkUp = (element, text) => {
+    
+    console.log(element);
 
+    const sel = window.getSelection();
+
+    // can we use the array and find the selected node in the array?
+    // the div.innerText splitten op /n?
+    console.log("sel.getRangeAt(0)", sel.getRangeAt(0).commonAncestorContainer);
+    console.log("text.length", text.length);
+    console.log(addTextboxText.innerText.split('\n'));
+    console.log(addTextboxText.innerText.split('\n').indexOf(sel.getRangeAt(0).commonAncestorContainer.data));
+
+    
     const startSelection = sel.anchorOffset > sel.focusOffset ? sel.focusOffset : sel.anchorOffset;
     const endSelection = sel.focusOffset > sel.anchorOffset ? sel.focusOffset : sel.anchorOffset;
-
+    
     // if ctrlA is used to select the text, the behaviour is different
     const ctrlA = sel.anchorNode.data ? false : true;
     const allText = ctrlA ? sel.anchorNode.textContent : sel.anchorNode.data;
+    
+    console.log("sel", sel.anchorNode.childNodes)
+    console.log("ctrlA", ctrlA)
+    console.log("allText", allText);
 
-    const selectedText = ctrlA ? allText : allText.slice(startSelection, endSelection);
-    const before = ctrlA ? '' : allText.slice(0, startSelection);
-    const after = ctrlA ? '' : allText.slice(endSelection);
+    const selectedText = ctrlA ? allText : text.slice(startSelection, endSelection);
+    const before = ctrlA ? '' : text.slice(0, startSelection);
+    const after = ctrlA ? '' : text.slice(endSelection);
+
+    console.log("before", before);
+    console.log("selectedText", selectedText);
+    console.log("after", after);
 
     if (startSelection === endSelection) {
         return;
     }
 
-    let text = '';
 
     switch (element) {
         case 'b':
@@ -60,15 +78,36 @@ const addMarkUp = (element) => {
         case 'p':
             text = `${before}<${element}>${selectedText}</${element}>${after}`;
             break;
+        case 'a':
+            text = `${before}<${element} href="" target="_blank">${selectedText}</${element}>${after}`;
+            break;
+        case 'img':
+            text = `${before}<${element} src="${selectedText}" alt="${addTitle.value}">${after}`;
+            break;
+        case 'div':
+            text = `${before}<${element} class="columns">${selectedText}</${element}>${after}`;
+            break;
+        case 'quote': 
+            text = `${before}<p class="page-content-quote">${selectedText}</p>${after}`;
+            break;
     }
+
+    console.log("text", text);
 
     addTextboxText.innerText = text;
 }
 
 addButtons.forEach(button => {
     button.addEventListener('click', (e) => {
-        addMarkUp(e.target.dataset.element);
-    })
+        e.preventDefault();
+        // const text = addTextboxText.innerText.replaceAll('\n', ' ');
+        const text = addTextboxText.innerText;
+
+        // break it
+        // setTimeOut(addMarkUp(e.target.dataset.element, multiLines), 3000);
+
+        addMarkUp(e.target.dataset.element, text);
+    });
 });
 
 addTextboxText.addEventListener('input', () => {
@@ -78,3 +117,7 @@ addTextboxText.addEventListener('input', () => {
 addTextboxText.addEventListener('change', () => {
     pageContentBody.innerHTML = addTextboxText.innerText;
 });
+
+submitButton.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
